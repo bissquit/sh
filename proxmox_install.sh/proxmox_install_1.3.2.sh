0282219ad930e24982d41fd3f865906daca56be5
root <bq@bissquit.com>
2017-11-03 14:50:25 +0300

    the last versions

--- proxmox_install.sh/proxmox_install_1.3.2.sh
+++ proxmox_install.sh/proxmox_install_1.3.2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #=========================================================================
-#         FILE: proxmox_install_1.3.1.sh
+#         FILE: proxmox_install_1.3.2.sh
 #
-#        USAGE: ./proxmox_install_1.3.1.sh [ preffered hostname ]
+#        USAGE: ./proxmox_install_1.3.2.sh [ preffered hostname ]
 #
 #  DESCRIPTION: Proxmox installation. Script has optimized for Hetzner's
 #               servers with single network interface.
@@ -10,7 +10,7 @@
 #        NOTES: 
 #       AUTHOR: E.S.Vasilyev - bq@bissquit.com; e.s.vasilyev@mail.ru
 #      VERSION: 1.3.1
-#      CREATED: 30.10.2017
+#      CREATED: 01.11.2017
 #=========================================================================
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -19,7 +19,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # common variables; do not include task-dependent params in this section
 #-------------------------------------------------------------------------
 
-log_file_name=$( sed 's/^\.\///' <<< $BASH_SOURCE.log ) # log file name
+log_file_name="$( sed 's/.*\/\(.*\)/\1/' <<< "$BASH_SOURCE.log" )" # log file name
 admin_email="bq@bissquit.com"				# admin's e-mails; use comma to separate addresses
 need_to_be_root=1					# need to be root? 1 - Yes, 0 - No
 
@@ -28,7 +28,7 @@ need_to_be_root=1					# need to be root? 1 - Yes, 0 - No
 #		e.d. [2017/10/19 11:02:39]:
 #=========================================================================
 function time_format {
-	printf '%s' "[$(date +"%Y/%m/%d %H:%M:%S")]:"
+	printf -- '%s' "[$(date +"%Y/%m/%d %H:%M:%S")]:"
 }
 
 #=========================================================================
@@ -36,15 +36,15 @@ function time_format {
 #  PARAMETER 1: package name (e.g. nano)
 #=========================================================================
 function check_package {
-	if [ 0 -eq "$( dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "install ok installed" )" ]; then
-		echo $(time_format) "$1 has not installed. Installation starts after 10 second!" >> "$log_file_name"
-		sleep 10
-		echo $(time_format) "Execute apt-get update" >> "$log_file_name"
+	if [ 0 -eq "$( dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "install ok installed" )" ]; then
+		printf -- '%s\n' "$(time_format) $1 has not installed. Installation starts after 10 second!" >> "$log_file_name"
+		sleep -- 10
+		printf -- '%s\n' "$(time_format) Execute apt-get update" >> "$log_file_name"
 		apt-get update >> "$log_file_name"
-		echo $(time_format) "$1 installation" >> "$log_file_name"
+		printf -- '%s\n' "$(time_format) $1 installation" >> "$log_file_name"
 		apt-get install -q -y "$1" >> "$log_file_name"
 	else
-		echo $(time_format) "$1 has been installed" >> "$log_file_name"
+		printf -- '%s\n' "$(time_format) $1 has been installed" >> "$log_file_name"
 	fi
 }
 
@@ -62,14 +62,14 @@ function execute_command {
     #check exit code
     if [ 0 -ne "$?" ]; then
 		if [ -z "$3" ]; then
-			echo $(time_format) "$1 - Error!!! Full command for debug: $2" >> "$log_file_name"
+			printf -- '%s\n' "$(time_format) $1 - Error!!! Full command for debug: $2" >> "$log_file_name"
 			mail -s "ERROR!!! $(hostname -f) - $BASH_SOURCE" "$admin_email" < "$log_file_name"
 			exit
 		else
-			echo $(time_format) "Previous command has returned error, but need to continue script execution" >> "$log_file_name"
+			printf -- '%s\n' "$(time_format) Previous command has returned error, but need to continue script execution" >> "$log_file_name"
 		fi
 	else
-		echo $(time_format) "$1 - Success" >> "$log_file_name"
+		printf -- '%s\n' "$(time_format) $1 - Success" >> "$log_file_name"
 	fi
 }
 
@@ -79,7 +79,7 @@ function execute_command {
 #=========================================================================
 function run_by_root {
 	if [ 0 -ne "$( id -u )" -a 1 -eq "${need_to_be_root}" ]; then
-		echo $(time_format) "Need to be root. Exit..." >> "$log_file_name"
+		printf -- '%s\n' "$(time_format) Need to be root. Exit..." >> "$log_file_name"
 		mail -s "Need to be root! $(hostname -f) - $BASH_SOURCE" "$admin_email" < "$log_file_name"
 		exit
 	fi
@@ -88,7 +88,7 @@ function run_by_root {
 #-------------------------------------------------------------------------
 # script start
 #-------------------------------------------------------------------------
-echo "$(time_format) $BASH_SOURCE starting work" >> "$log_file_name"
+printf -- '%s\n' "$(time_format) $BASH_SOURCE starting work" >> "$log_file_name"
 run_by_root
 
 #-------------------------------------------------------------------------
@@ -100,83 +100,84 @@ run_by_root
 proxmox_hostname="$1"				# set new hostname
 iso_storage_path="/var/iso"			# path for iso image storage
 firewall_script_path="./firewall_proxmox.sh"	# path to optional iptables script
-if_name=$( sed -r '/auto [^lo]/!d;s/auto (.*)/\1/' /etc/network/interfaces ) # get interface name
+if_name="$( sed -r '/auto [^lo]/!d;s/auto (.*)/\1/' /etc/network/interfaces )" # get interface name
 
 #-------------------------------------------------------------------------
 # redefine /etc/hosts ( need for proper Proxmox installation ) and
 # change hostname
 #-------------------------------------------------------------------------
-echo $(time_format) "Change hostname to $proxmox_hostname" >> "$log_file_name"
-sed -i 's/'"$(hostname)"'/'"$proxmox_hostname"'/' /etc/hosts
-echo "$proxmox_hostname" > /etc/hostname
+printf -- '%s\n' "$(time_format) Change hostname to $proxmox_hostname" >> "$log_file_name"
+# bash variable substitution in sed: be carefull with regex patterns in filename
+sed -i 's/'"$(hostname)"'/'"$proxmox_hostname"'/' -- "/etc/hosts"
+printf -- '%s\n' "$proxmox_hostname" > "/etc/hostname"
 execute_command "Run hostnamectl" "hostnamectl set-hostname $proxmox_hostname"
 
 #-------------------------------------------------------------------------
 # add repo
 #-------------------------------------------------------------------------
-echo "deb http://download.proxmox.com/debian/pve stretch pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-wget http://download.proxmox.com/debian/proxmox-ve-release-5.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-5.x.gpg
+printf -- '%s\n' "deb http://download.proxmox.com/debian/pve stretch pve-no-subscription" > "/etc/apt/sources.list.d/pve-install-repo.list"
+wget -- http://download.proxmox.com/debian/proxmox-ve-release-5.x.gpg -O "/etc/apt/trusted.gpg.d/proxmox-ve-release-5.x.gpg"
 
 #-------------------------------------------------------------------------
 # create answer files
 #-------------------------------------------------------------------------
-echo "postfix postfix/main_mailer_type string Internet site" > postfix_silent_install.txt
-echo "postfix postfix/mailname string localhost.localdomain" >> postfix_silent_install.txt
-echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" > iptables-persistent_silent_install.txt
-echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" >> iptables-persistent_silent_install.txt
-debconf-set-selections postfix_silent_install.txt
-debconf-set-selections iptables-persistent_silent_install.txt
+printf -- '%s\n' "postfix postfix/main_mailer_type string Internet site" > "postfix_silent_install.txt"
+printf -- '%s\n' "postfix postfix/mailname string localhost.localdomain" >> "postfix_silent_install.txt"
+printf -- '%s\n' "iptables-persistent iptables-persistent/autosave_v4 boolean true" > "iptables-persistent_silent_install.txt"
+printf -- '%s\n'  "iptables-persistent iptables-persistent/autosave_v6 boolean true" >> "iptables-persistent_silent_install.txt"
+debconf-set-selections -- "postfix_silent_install.txt"
+debconf-set-selections -- "iptables-persistent_silent_install.txt"
 
 #-------------------------------------------------------------------------
 # Proxmox installation
 #-------------------------------------------------------------------------
 execute_command "Repo update" "apt-get update"
-#execute_command "Package update" "apt-get dist-upgrade -y"
 DEBIAN_FRONTEND='noninteractive' apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade >> "$log_file_name"
-#DEBIAN_FRONTEND=noninteractive
 execute_command "Proxmox installation" "apt-get install -y proxmox-ve postfix open-iscsi"
 
 #-------------------------------------------------------------------------
 # bridge configuring
 #-------------------------------------------------------------------------
-echo $(time_format) "Bridge configure" >> "$log_file_name"
-cat /etc/network/interfaces > /etc/network/interfaces.backup
-sed -i 's/'"$if_name"'/vmbr0/' /etc/network/interfaces
-sed -i 's/auto vmbr0/auto vmbr0\nallow-hotplug vmbr0/' /etc/network/interfaces
-sed -i '/iface vmbr0 inet.*/ a\  bridge_ports '"$if_name"'' /etc/network/interfaces
-echo "" >> /etc/network/interfaces
-echo "auto $if_name" >> /etc/network/interfaces
-echo "allow-hotplug $if_name" >> /etc/network/interfaces
-echo "iface $if_name inet manual" >> /etc/network/interfaces
+printf -- '%s\n' "$(time_format) Bridge configure" >> "$log_file_name"
+cat "/etc/network/interfaces" > "/etc/network/interfaces.backup"
+# bash variable substitution in sed: be carefull with regex patterns in filename
+sed -i 's/'"$if_name"'/vmbr0/' -- "/etc/network/interfaces"
+sed -i 's/auto vmbr0/auto vmbr0\nallow-hotplug vmbr0/' -- "/etc/network/interfaces"
+sed -i '/iface vmbr0 inet.*/ a\  bridge_ports '"$if_name"'' -- "/etc/network/interfaces"
+printf -- '%s\n' "" >> "/etc/network/interfaces"
+printf -- '%s\n' "auto $if_name" >> "/etc/network/interfaces"
+printf -- '%s\n' "allow-hotplug $if_name" >> "/etc/network/interfaces"
+printf -- '%s\n' "iface $if_name inet manual" >> "/etc/network/interfaces"
 
 #-------------------------------------------------------------------------
 # create local iso storage
 # https://pve.proxmox.com/wiki/Storage
 # https://pve.proxmox.com/pve-docs/pvesm.1.html
 #-------------------------------------------------------------------------
-echo $(time_format) "Configuring local iso storage" >> "$log_file_name"
-mkdir "$iso_storage_path"
-execute_command "Configure iso storage" "pvesm add dir iso --path ${iso_storage_path} --content iso" "1"
-echo $(time_format) "INFO: Put your iso images in ${iso_storage_path}/template/iso" >> "$log_file_name"
+printf -- '%s\n' "$(time_format) Configuring local iso storage" >> "$log_file_name"
+mkdir -- "$iso_storage_path"
+execute_command "Configure iso storage" "pvesm add dir iso --path $iso_storage_path --content iso" "1"
+#pvesm add dir iso --path "${iso_storage_path}" --content iso
+printf -- '%s\n' "$(time_format) INFO: Put your iso images in ${iso_storage_path}/template/iso" >> "$log_file_name"
 
 #-------------------------------------------------------------------------
 # iptables definition. This step will be omitted if file
 # $firewall_script_path does not exist
 #-------------------------------------------------------------------------
 check_package "iptables-persistent"
-if [ -f "$firewall_script_path" ] ; then
-	echo $(time_format) "Configure firewall rules" >> "$log_file_name"
+if [ -f "$firewall_script_path" ]; then
+	printf -- '%s\n' "$(time_format) Configure firewall rules" >> "$log_file_name"
 	bash "$firewall_script_path" 2>> "$log_file_name"
-	iptables-save > /etc/iptables/rules.v4 2>> "$log_file_name"
-	ip6tables-save > /etc/iptables/rules.v6 2>> "$log_file_name"
-	echo $(time_format) "Iptables configuring has finished. Done..." >> "$log_file_name"
+	iptables-save > "/etc/iptables/rules.v4" 2>> "$log_file_name"
+	ip6tables-save > "etc/iptables/rules.v6" 2>> "$log_file_name"
+	printf -- '%s\n' "$(time_format) Iptables configuring has finished. Done..." >> "$log_file_name"
 else
-	echo $(time_format) "WARNING!!! Firewall script is missing. Any rules does not apply" >> "$log_file_name"
+	printf -- '%s\n' "$(time_format) WARNING!!! Firewall script is missing. Any rules does not apply" >> "$log_file_name"
 fi
 
 #-------------------------------------------------------------------------
 # script finished
 #-------------------------------------------------------------------------
-echo $(time_format) "$BASH_SOURCE has finished work. Reboot..." >> "$log_file_name"
-mail -s $(time_format) "123" "$admin_email" < "$log_file_name"
+printf -- '%s\n' "$(time_format) $BASH_SOURCE has finished work. Reboot..." >> "$log_file_name"
+mail -s "$(time_format) 123" "$admin_email" < "$log_file_name"
 reboot
--- xen_install.sh/xen_install_1.2.4.sh
+++ xen_install.sh/xen_install_1.2.4.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #=========================================================================
-#         FILE: xen_install_V1.2.3.sh
+#         FILE: xen_install_V1.2.4.sh
 #
-#        USAGE: ./xen_install_V1.2.3.sh
+#        USAGE: ./xen_install_V1.2.4.sh
 #
 #  DESCRIPTION: perform Xen installation. Script has optimized for Hetzner
 #
 #        NOTES: 
 #       AUTHOR: E.S.Vasilyev - bq@bissquit.com; e.s.vasilyev@mail.ru
-#      VERSION: 1.2.3
-#      CREATED: 31.10.2017
+#      VERSION: 1.2.4
+#      CREATED: 01.11.2017
 #=========================================================================
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -18,7 +18,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # common variables; do not include task-dependent params in this section
 #-------------------------------------------------------------------------
 
-log_file_name=$( sed 's/^\.\///' <<< $BASH_SOURCE.log ) # log file name
+log_file_name="$( sed 's/.*\/\(.*\)/\1/' <<< "$BASH_SOURCE.log" )" # log file name
 admin_email="bq@bissquit.com"				# admin's e-mails; use comma to separate addresses
 need_to_be_root=1					# need to be root? 1 - Yes, 0 - No
 
@@ -27,7 +27,7 @@ need_to_be_root=1					# need to be root? 1 - Yes, 0 - No
 #		e.d. [2017/10/19 11:02:39]:
 #=========================================================================
 function time_format {
-	printf '%s' "[$(date +"%Y/%m/%d %H:%M:%S")]:"
+	printf -- '%s' "[$(date +"%Y/%m/%d %H:%M:%S")]:"
 }
 
 #=========================================================================
@@ -35,15 +35,15 @@ function time_format {
 #  PARAMETER 1: package name (e.g. nano)
 #=========================================================================
 function check_package {
-	if [ 0 -eq "$( dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "install ok installed" )" ]; then
-		echo $(time_format) "$1 has not installed. Installation starts after 10 second!" >> "$log_file_name"
-		sleep 10
-		echo $(time_format) "Execute apt-get update" >> "$log_file_name"
+	if [ 0 -eq "$( dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "install ok installed" )" ]; then
+		printf -- '%s\n' "$(time_format) $1 has not installed. Installation starts after 10 second!" >> "$log_file_name"
+		sleep -- 10
+		printf -- '%s\n' "$(time_format) Execute apt-get update" >> "$log_file_name"
 		apt-get update >> "$log_file_name"
-		echo $(time_format) "$1 installation" >> "$log_file_name"
+		printf -- '%s\n' "$(time_format) $1 installation" >> "$log_file_name"
 		apt-get install -q -y "$1" >> "$log_file_name"
 	else
-		echo $(time_format) "$1 has been installed" >> "$log_file_name"
+		printf -- '%s\n' "$(time_format) $1 has been installed" >> "$log_file_name"
 	fi
 }
 
@@ -61,14 +61,14 @@ function execute_command {
     #check exit code
     if [ 0 -ne "$?" ]; then
 		if [ -z "$3" ]; then
-			echo $(time_format) "$1 - Error!!! Full command for debug: $2" >> "$log_file_name"
+			printf -- '%s\n' "$(time_format) $1 - Error!!! Full command for debug: $2" >> "$log_file_name"
 			mail -s "ERROR!!! $(hostname -f) - $BASH_SOURCE" "$admin_email" < "$log_file_name"
 			exit
 		else
-			echo $(time_format) "Previous command has returned error, but need to continue script execution" >> "$log_file_name"
+			printf -- '%s\n' "$(time_format) Previous command has returned error, but need to continue script execution" >> "$log_file_name"
 		fi
 	else
-		echo $(time_format) "$1 - Success" >> "$log_file_name"
+		printf -- '%s\n' "$(time_format) $1 - Success" >> "$log_file_name"
 	fi
 }
 
@@ -78,7 +78,7 @@ function execute_command {
 #=========================================================================
 function run_by_root {
 	if [ 0 -ne "$( id -u )" -a 1 -eq "${need_to_be_root}" ]; then
-		echo $(time_format) "Need to be root. Exit..." >> "$log_file_name"
+		printf -- '%s\n' "$(time_format) Need to be root. Exit..." >> "$log_file_name"
 		mail -s "Need to be root! $(hostname -f) - $BASH_SOURCE" "$admin_email" < "$log_file_name"
 		exit
 	fi
@@ -87,7 +87,7 @@ function run_by_root {
 #-------------------------------------------------------------------------
 # script start
 #-------------------------------------------------------------------------
-echo "$(time_format) $BASH_SOURCE starting work" >> "$log_file_name"
+printf -- '%s\n' "$(time_format) $BASH_SOURCE starting work" >> "$log_file_name"
 run_by_root
 
 #-------------------------------------------------------------------------
@@ -98,12 +98,12 @@ run_by_root
 #-------------------------------------------------------------------------
 # heck hardware virtualization support
 #-------------------------------------------------------------------------
-echo "${time_format} Check hardware virtualization support"
+printf -- '%s\n' "$(time_format) Check hardware virtualization support"
 if [ 0 -eq "$( egrep '(vmx|svm)' /proc/cpuinfo | wc -l )" ]; then
-	echo "${time_format} Warning!!! This server haven't hardware virtualization support" >> "${log_file_name}"
+	printf -- '%s\n' "$(time_format) Warning!!! This server haven't hardware virtualization support" >> "$log_file_name"
 fi
 
-echo "${time_format} Info! This script has optimized for configuring Hetzner's default Debian installation" >> "${log_file_name}"
+printf -- '%s\n' "$(time_format) Info! This script has optimized for configuring Hetzner's default Debian installation" >> "$log_file_name"
 
 #-------------------------------------------------------------------------
 # Xen installation
@@ -111,24 +111,24 @@ echo "${time_format} Info! This script has optimized for configuring Hetzner's d
 execute_command "Install XEN" "apt-get -q -y install xen-linux-system-amd64 xen-tools"
 execute_command "Configure GRUB" "dpkg-divert --divert /etc/grub.d/08_linux_xen --rename /etc/grub.d/20_linux_xen"
 execute_command "Update GRUB" "update-grub"
-echo "${time_format} Warning! Reboot needed!" >> "${log_file_name}"
-echo "${time_format} Xen installation has finished. Done..." >> "${log_file_name}"
+printf -- '%s\n' "$(time_format) Warning! Reboot needed!" >> "$log_file_name"
+printf -- '%s\n' "$(time_format) Xen installation has finished. Done..." >> "$log_file_name"
 
 #-------------------------------------------------------------------------
 # Network configuring
 #-------------------------------------------------------------------------
-echo "${time_format} Network configure starting..." >> "${log_file_name}"
+printf -- '%s\n' "$(time_format) Network configure starting..." >> "$log_file_name"
 execute_command "Backup network settings" "mv /etc/network/interfaces /etc/network/interfaces.backup"
-echo "${time_format} Configuring /etc/network/interfaces" >> "${log_file_name}"
-cat /etc/network/interfaces.backup > /etc/network/interfaces
-sed -i 's/eth0/xenbr0/' /etc/network/interfaces
-sed -i '/iface xenbr0 inet static/ a\  bridge_ports eth0' /etc/network/interfaces
-echo "" >> /etc/network/interfaces
-echo "auto eth0" >> /etc/network/interfaces
-echo "iface eth0 inet manual" >> /etc/network/interfaces
-echo "${time_format} Network configuring has finished. Done..." >> "${log_file_name}"
+printf -- '%s\n' "$(time_format) Configuring /etc/network/interfaces" >> "$log_file_name"
+cat -- "/etc/network/interfaces.backup" > "/etc/network/interfaces"
+sed -i 's/eth0/xenbr0/' -- "/etc/network/interfaces"
+sed -i '/iface xenbr0 inet static/ a\  bridge_ports eth0' -- "/etc/network/interfaces"
+printf -- '%s\n' "" >> "/etc/network/interfaces"
+printf -- '%s\n' "auto eth0" >> "/etc/network/interfaces"
+printf -- '%s\n' "iface eth0 inet manual" >> "/etc/network/interfaces"
+printf -- '%s\n' "$(time_format) Network configuring has finished. Done..." >> "$log_file_name"
 
-echo "$(time_format) $BASH_SOURCE has finished work. Reboot..." >> "${log_file_name}"
+printf -- '%s\n' "$(time_format) $BASH_SOURCE has finished work. Reboot..." >> "$log_file_name"
 
 #-------------------------------------------------------------------------
 # script finished
